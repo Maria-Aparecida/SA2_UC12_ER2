0338919c5f121dad968e0a8c9e12171f7a3c5546
Maria Aparecida <aparecidasousaferreira@gmail.com>
2022-03-21 22:34:21 -0300

    Encontro Remoto 3

--- Classes/PessoaFisica.cs
+++ Classes/PessoaFisica.cs
@@ -5,15 +5,51 @@ namespace Cadastro_Pessoa.Classes
     public class PessoaFisica : Pessoa, IPessoaFisica
     {
         public string ?cpf { get; set; }
-        public DateTime ?dataNascimento { get; set; }
+        public string ?dataNascimento { get; set; }
         
         
         
         
         public bool validarDataNascimento(DateTime dataNascimento)
         {
-            throw new NotImplementedException();
+            DateTime dataAtual = DateTime.Today;
+            double anos = (dataAtual - dataNascimento).TotalDays/365;
+            if(anos>= 18){
+                Console.WriteLine($"{dataNascimento}");
+                
+                return true;
+            }
+            return false;
+            
         }
+
+        
+        public bool validarDataNascimento(string dataNascimento)
+        {
+
+            DateTime dataConvertida;
+            if(DateTime.TryParse(dataNascimento, out dataConvertida)){
+
+                Console.WriteLine($"{dataConvertida}");
+                
+
+                 DateTime dataAtual = DateTime.Today;
+                double anos = (dataAtual - dataConvertida).TotalDays/365;
+            if(anos>= 18){
+                return true;
+            }
+            return false;
+            
+        }
+        return false;
+
+
+    }
+
+
+           
+
+
         public override float PagarImposto(float rendimento)
         {
             throw new NotImplementedException();
--- Program.cs
+++ Program.cs
@@ -1,9 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Cadastro_Pessoa.Classes;
+PessoaFisica metodoPF= new PessoaFisica();
 
 PessoaFisica novaPF = new PessoaFisica();
+Endereco novoEnd =  new Endereco();
 
-novaPF.nome = "Cida";
+novaPF.nome = "Aparecida";
+novaPF.dataNascimento = ("13/08;/1987");
+novaPF.cpf = "12345678901";
+novaPF.rendimento = 1854.5f;
 
-Console.WriteLine("Nome: "+novaPF.nome);
-Console.WriteLine($"Nome: {novaPF.nome}");
+novoEnd.logradouro = "Rua das Flores";
+novoEnd.numero = 12345;
+novoEnd.complemento = "Comercial Violeta";
+novoEnd.endComercial = true;
+
+novaPF.endereco = novoEnd;
+
+
+//Console.WriteLine($"Nome:{novaPF.nome}");
+//Console.WriteLine($"Data de Nascimento:{novaPF.dataNascimento}");
+//Console.WriteLine($"CPF:{novaPF.cpf}");
+//Console.WriteLine($"Rendimento:{novaPF.rendimento}");
+//Console.WriteLine($"Endereço: {novaPF.endereco.logradouro},{novaPF.endereco.numero}");
+
+Console.WriteLine(@$"
+Nome:{novaPF.nome}
+Maior de idade:{ metodoPF.validarDataNascimento(novaPF.dataNascimento)}
+CPF:{novaPF.cpf}
+Rendimento:{novaPF.rendimento}
+Endereço: {novaPF.endereco.logradouro},{novaPF.endereco.numero}
+");
+
+
+
