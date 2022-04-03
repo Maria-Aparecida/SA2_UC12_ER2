80f7ea940a3e9e05a6df93d4dd5dac6c3d51c4c9
Maria-Aparecida <88636424+Maria-Aparecida@users.noreply.github.com>
2022-04-03 13:39:25 -0300

    "Encontro Rmoto 4"

---
+++ PessoaJuridica.cs
@@ -0,0 +1,45 @@
+using System.Text.RegularExpressions;
+using Cadastro_Pessoa.Interfaces;
+
+namespace Cadastro_Pessoa.Classes
+{
+    public class PessoaJuridica : Pessoa, IPessoaJuridica
+    {
+
+        public string ?cnpj { get; set; }
+        public string ?razaoSocial { get; set; }
+        
+        
+        
+        
+
+        public override float PagarImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
+        //xx.xxx.xxx/0001-xx    xxxxxxxx0001xx
+        public bool ValidarCnpj(string cnpj)
+        {
+            if(Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)"))
+            {
+                if(cnpj.Length == 18)
+                {
+                    if(cnpj.Substring(11,4 )== "0001")
+                    {
+                        return true;
+                    }
+                    
+                   
+                }
+                    else if(cnpj.Length == 14)
+                    {
+                        if(cnpj.Substring(8,4) == "0001"){
+                             return true;
+                        }
+                        
+                    }
+            }
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Cadastro_Pessoa.Classes;
-PessoaFisica metodoPF= new PessoaFisica();
+// PessoaFisica metodoPF= new PessoaFisica();
 
-PessoaFisica novaPF = new PessoaFisica();
-Endereco novoEnd =  new Endereco();
+// PessoaFisica novaPF = new PessoaFisica();
+// Endereco novoEnd =  new Endereco();
 
-novaPF.nome = "Aparecida";
-novaPF.dataNascimento = ("13/08;/1987");
-novaPF.cpf = "12345678901";
-novaPF.rendimento = 1854.5f;
+// novaPF.nome = "Aparecida";
+// novaPF.dataNascimento = ("13/08;/1987");
+// novaPF.cpf = "12345678901";
+// novaPF.rendimento = 1854.5f;
 
-novoEnd.logradouro = "Rua das Flores";
-novoEnd.numero = 12345;
-novoEnd.complemento = "Comercial Violeta";
-novoEnd.endComercial = true;
+// novoEnd.logradouro = "Rua das Flores";
+// novoEnd.numero = 12345;
+// novoEnd.complemento = "Comercial Violeta";
+// novoEnd.endComercial = true;
 
-novaPF.endereco = novoEnd;
+// novaPF.endereco = novoEnd;
 
 
 //Console.WriteLine($"Nome:{novaPF.nome}");
@@ -24,13 +24,39 @@ novaPF.endereco = novoEnd;
 //Console.WriteLine($"Rendimento:{novaPF.rendimento}");
 //Console.WriteLine($"Endereço: {novaPF.endereco.logradouro},{novaPF.endereco.numero}");
 
+// Console.WriteLine(@$"
+// Nome:{novaPF.nome}
+// Maior de idade:{ metodoPF.validarDataNascimento(novaPF.dataNascimento)}
+// CPF:{novaPF.cpf}
+// Rendimento:{novaPF.rendimento}
+// Endereço: {novaPF.endereco.logradouro},{novaPF.endereco.numero}
+// ");
+
+PessoaJuridica metodoPj= new PessoaJuridica();
+
+
+PessoaJuridica novaPj = new PessoaJuridica();
+Endereco novoEndPj = new Endereco();
+
+novaPj.nome = "Nome Pj";
+novaPj.cnpj = "00111222000133";
+novaPj.razaoSocial = "Razão Social PJ";
+novaPj.rendimento = 30000.5f;
+
+novoEndPj.logradouro = "Rua das Violetas";
+novoEndPj.numero = 1313;
+novoEndPj.complemento = "Palácio do Comércio";
+novoEndPj.endComercial = true;
+
+novaPj.endereco = novoEndPj;
+
+ //Console.WriteLine($"{metodoPj.validarCnpj("00111222000133")}");
+
 Console.WriteLine(@$"
-Nome:{novaPF.nome}
-Maior de idade:{ metodoPF.validarDataNascimento(novaPF.dataNascimento)}
-CPF:{novaPF.cpf}
-Rendimento:{novaPF.rendimento}
-Endereço: {novaPF.endereco.logradouro},{novaPF.endereco.numero}
-");
+Nome: {novaPj.nome}
+Razão Social: {novaPj.razaoSocial}
+CNPJ: {novaPj.cnpj}
+CNPJ Válido: {metodoPj.ValidarCnpj(novaPj.cnpj)}");
 
 
 
